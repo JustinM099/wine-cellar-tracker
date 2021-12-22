babd0e9eaeacd03d9d7478992bb7d9460efcd495
Nikko <88990996+neex18@users.noreply.github.com>
2021-12-21 20:49:04 -0800

    added create new user route in userRoutes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,27 @@
 const router = require('express').Router();
 const { User } = require('../../models/User');
 
+
+//create new user
+router.post('/', async (req, res) => {
+  try {
+    const userData = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    req.session.save(() => {
+      req.session.loggedIn = true;
+
+      res.status(200).json(dbUserData);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
 router.post('/login', async (req, res) => {
   try {
     // TODO: Add a comment describing the functionality of this expression
