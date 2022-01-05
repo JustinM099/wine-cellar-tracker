e18a06b6d5581d39a2f56f8ae48c201f6a74a11d
JustinM099 <justinm099@mac.com>
2022-01-05 00:13:39 -0800

    updating bottle seeds

--- seeds/bottle-seeds.js
+++ seeds/bottle-seeds.js
@@ -7,15 +7,96 @@ const bottleData = [
         vintage: 1961,
         region: 'Bordeaux',
         tasting_notes: 'Old band-aid and pencil shavings. 100 points.',
-        wine_type: 'red',
+        wine_type: 'Red Wine',
         cellar_location: 'B01',
         stock: 12,
-        category_id: 1,
+        category_id: 31,
         user_id: 1,
         reviews: '99 Points Wine Advocate',
         wine_name: 'Grand Vin'
 
     },
+    {
+        producer_name: 'Chateau Latour',
+        vintage: 1982,
+        region: 'Bordeaux',
+        tasting_notes: 'Black currants, cedar box, and dried herbs.',
+        wine_type: 'Red Wine',
+        cellar_location: 'B01',
+        stock: 17,
+        category_id: 31,
+        user_id: 1,
+        reviews: '100 Points Wine Advocate',
+        wine_name: 'Grand Vin'
+    },
+    {
+        producer_name: 'Chateau Latour',
+        vintage: 2000,
+        region: 'Bordeaux',
+        tasting_notes: 'Blackberry, toasted vanilla, and forest floor.',
+        wine_type: 'Red Wine',
+        cellar_location: 'B01',
+        stock: 24,
+        category_id: 31,
+        user_id: 1,
+        reviews: '100 Points Wine Spectator',
+        wine_name: 'Grand Vin'
+    },
+    {
+        producer_name: 'Sine Qua Non',
+        vintage: 2015,
+        region: 'Santa Barbara County',
+        tasting_notes: 'Grilled meats, wood smoke, truffle, and minerals.',
+        wine_type: 'Red Wine',
+        cellar_location: 'A11',
+        stock: 6,
+        category_id: 32,
+        user_id: 1,
+        reviews: '100 Points Wine Advocate',
+        wine_name: 'Eleven Confessions'
+    },
+    {
+        producer_name: 'Dom Perignon',
+        vintage: 2012,
+        region: 'Champagne',
+        tasting_notes: 'Pears, green apples, brioche, cream and a hint of honey.',
+        wine_type: 'Sparkling Wine',
+        cellar_location: 'C09',
+        stock: 11,
+        category_id: 30,
+        user_id: 1,
+        reviews: '96 Points Wine Spectator',
+        wine_name: 'Brut'
+    },
+    {
+        producer_name: 'Hanzell',
+        vintage: 2015,
+        region: 'Sonoma Valley',
+        tasting_notes: 'Pineapple, apple pie, and nectarine.',
+        wine_type: 'White Wine',
+        cellar_location: 'D05',
+        stock: 9,
+        category_id: 9,
+        user_id: 1,
+        reviews: '100 Points Wine Advocate',
+        wine_name: 'Chardonnay'
+    },
+    {
+        producer_name: `Dow's`,
+        vintage: 1994,
+        region: 'Port',
+        tasting_notes: 'Raspberries, raisins, prunes, flowers, and nutmeg',
+        wine_type: 'Dessert Wine',
+        cellar_location: 'C02',
+        stock: 9,
+        category_id: 35,
+        user_id: 1,
+        reviews: '97 Points Wine Spectator',
+        wine_name: 'Grand Vin'
+    },
+
+
+
 
 ]
 
