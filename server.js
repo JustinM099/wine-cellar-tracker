a21e3b42d05d9ff32e957baaae2839257ea5306f
Nikko <88990996+neex18@users.noreply.github.com>
2022-01-05 21:13:32 -0800

    Added to nodemailer

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 console.log(' PR testing!')
 // added for nodemailer
-// const bodyParser = require('body-parser');
-// const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -38,19 +38,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 // body parser middleware
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use(routes);
-
-// added for nodemailer
-app.get('/', (req, res) => {
-  res.render('contact');
-});
-
-app.get('/', (req, res) => {
-  res.send('');
-});
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
@@ -58,4 +49,56 @@ sequelize.sync({ force: false }).then(() => {
 
 
 
-// app.listen(3000, () => console.log('server started'));
+// added for nodemailer
+app.get('/', (req, res) => {
+  res.render('login');
+});
+
+app.post('/send', (req, res) => {
+  const output = `
+  <P>You have a new sign up</p>
+  <h3>Contact Details</h3>
+  <ul>
+    <li>${req.body.name}</li>
+    <li>${req.body.email}</li>
+  </ul>
+  `;
+  // create reusable transporter object using the default SMTP transport
+  async function main() {
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 456,
+    secure: true, // true for 465, false for other ports
+    auth: {
+      user: 'uwproject2@gmail.com', // generated ethereal user
+      pass: 'Password456', // generated ethereal password
+    },
+    tls:{
+      rejectUnathorized:false
+    }
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"Nodemailer Contact" <uwproject2@gmail.com>', // sender address
+    to: 'uwproject2@gmail.com',  // list of receivers, we are just using the same email address
+    subject: "Node New Contact", // Subject line
+    text: "Hello world?", // plain text body
+    html: output // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  
+}
+
+main().catch(console.error);
+
+});
+
+
+
+
+
+
+
