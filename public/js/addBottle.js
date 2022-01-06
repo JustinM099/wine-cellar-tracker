ec0741fb6993bd3245c2af071f311ae5b808ed61
JustinM099 <justinm099@mac.com>
2022-01-06 12:51:22 -0800

    fixing bottle POST

--- public/js/addBottle.js
+++ public/js/addBottle.js
@@ -9,11 +9,12 @@ const newBottle = async (e) => {
     const vintage = document.querySelector('#vintage').value
     const region = document.querySelector('#region').value
     const wine_type = document.querySelector('#wine-type').value //this isnt working
-    const category_id = document.querySelector('#variety').toString().value //this isnt going to work
+    const category_id = document.querySelector('#variety').value //this isnt going to work
     const reviews = document.querySelector('#reviews').value
     const tasting_notes = document.querySelector('#tasting-notes').value
     const stock = document.querySelector('#stock').value
     const cellar_location = document.querySelector('#cellar-location').value
+
 
     if (producer_name && wine_type && category_id && stock) {
         const response = await fetch(`/api/bottle`, {
@@ -36,10 +37,16 @@ const newBottle = async (e) => {
         })
         console.log('Response', response)
         if (response.ok) {
-            document.location.replace('/homepage');
+            document.location.replace('/');
         } else {
             alert(`I'm sorry, we were unable to create your bottle. Please try again.`);
         }
+    } else {
+        console.log('Houston, we have a problem.', 
+        'producer name:', producer_name,
+        'wine_type:', wine_type,
+        'category_id', category_id,
+        'stock', stock)
     }
 }
 document.querySelector('#bottle-button').addEventListener('click', newBottle)
